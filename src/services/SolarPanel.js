d52ca62184cc84b0ad410fed39729fb39e031865
amydonglilifun <amy.donglili@gmail.com>
2024-05-16 12:25:12 -0700

    Update SolarPanel.js

--- src/services/SolarPanel.js
+++ src/services/SolarPanel.js
@@ -3,11 +3,34 @@ class SolarPanel {
       this.temperature = 10; // in degrees Celsius
     }
   
-    absorbSunlight(sunlightIntensity) { // sunlightIntensity is between 0 and 1
-      this.temperature += sunlightIntensity; // increase temperature based on sunlight intensity
+    absorbSunlight(timeOfDay) {
+      var sunlightIntensity = this.calculateSunlightIntensity(timeOfDay);
+      this.temperature += this.calculateSolarTemperature(sunlightIntensity, timeOfDay); // increase temperature based on sunlight intensity
     }
 
     temperature() {
       return this.temperature > 99 ? 99 : this.temperature;
+    }
+
+    // calculate sunlight intensity based on time of day
+    calculateSunlightIntensity(timeOfDay) {
+      const maxIntensity = 1000; // in watts per square meter
+      const hoursAfterNoon = Math.abs(timeOfDay - 12);
+      const intensityDecreaseRate = maxIntensity / 12;
+      let sunlightIntensity = maxIntensity - (intensityDecreaseRate * hoursAfterNoon);
+      return sunlightIntensity;
+    }
+
+    // To calculate the solar temperature based on sunlight intensity and time,
+    // we will use the Stefan-Boltzmann Law. 
+    // This law states that the total energy radiated per unit surface area of a black body is 
+    // directly proportional to the fourth power of the black body's temperature.
+    calculateSolarTemperature(sunlightIntensity, timeOfDay) {
+      const stefanBoltzmannConstant = 5.67 * Math.pow(10, -8);
+      let energy = sunlightIntensity * timeOfDay;
+      console.log("energy: " + energy);
+      let temperature = Math.pow((energy / stefanBoltzmannConstant), 0.25);
+      console.log("temperature: " + temperature);
+      return temperature;
     }
   }
